4324751b61c456663d5f3ab49ddf3ff8cf195a82
Ujjawal Gupta <gujjawal29@gmail.com>
2024-05-29 16:31:30 +0530

    Zookeeper setup

---
+++ Zookeeper/src/main/java/LeaderElection.java
@@ -0,0 +1,48 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class LeaderElection implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private ZooKeeper zooKeeper;
+    private static final int SESSION_TIMEOUT = 3000;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connectToZookeeper();
+        leaderElection.run();
+        leaderElection.close();
+        System.out.println("Disconnected from Zookeeper, exiting app");
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        switch (watchedEvent.getType()) {
+            case None :
+                if(watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Recieved Disconnected event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+        }
+    }
+}

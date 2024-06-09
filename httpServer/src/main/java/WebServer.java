414a50bc8be43884fb3338f8b569c9b7a6b7704d
Ujjawal Gupta <gujjawal29@gmail.com>
2024-06-09 16:46:48 +0530

    httpServer

---
+++ httpServer/src/main/java/WebServer.java
@@ -0,0 +1,118 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.IncompleteAnnotationException;
+import java.math.BigInteger;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class WebServer {
+    private static final String TASK_ENDPOINT = "/task";
+    private static final String STATUS_ENDPOINT = "/status";
+    private final int port;
+    private HttpServer httpServer;
+
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        int serverPort = 8080;
+        if(args.length == 1) {
+            serverPort = Integer.parseInt(args[0]);
+        }
+
+        WebServer webServer = new WebServer(serverPort);
+        webServer.startServer();
+
+        System.out.println("Server is listening on port: " + serverPort);
+    }
+
+    public void startServer() {
+        try {
+            this.httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        HttpContext statusContext = httpServer.createContext(STATUS_ENDPOINT);
+        HttpContext taskContext = httpServer.createContext(TASK_ENDPOINT);
+
+        statusContext.setHandler(this::handleStatusCheckRequest);
+        taskContext.setHandler(this::handleTaskRequest);
+
+        httpServer.setExecutor(Executors.newFixedThreadPool(8));
+        httpServer.start();
+    }
+
+    private void handleTaskRequest(HttpExchange exchange) throws IOException {
+        if(!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+
+        Headers headers = exchange.getRequestHeaders();
+        if(headers.containsKey("X-Test") && headers.get("X-Test").get(0).equalsIgnoreCase("true")) {
+            String dummyResponse = "123\n";
+            sendResponse(dummyResponse.getBytes(), exchange);
+            return;
+        }
+
+        boolean isDebugMode = false;
+        if(headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
+            isDebugMode = true;
+        }
+
+        long startTime = System.nanoTime();
+
+        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+        byte[] responseBytes = calculateResponse(requestBytes);
+
+        long finishTime = System.nanoTime();
+        if(isDebugMode) {
+            String debugMsg = String.format("Operation took %d ns", finishTime - startTime);
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMsg));
+        }
+
+        sendResponse(responseBytes, exchange);
+    }
+
+    private byte[] calculateResponse(byte[] requestBytes) {
+        String bodyString = new String(requestBytes);
+        String[] stringNumbers = bodyString.split(",");
+
+        BigInteger result = BigInteger.ONE;
+
+        for(String num : stringNumbers) {
+            BigInteger number = new BigInteger(num);
+            result = result.multiply(number);
+        }
+
+        return String.format("Result of multiplication is %s\n", result).getBytes();
+    }
+
+    private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
+        if(!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            exchange.close();
+            return;
+        }
+        
+        String responseMsg = "Server is Alive";
+        sendResponse(responseMsg.getBytes(), exchange);
+    }
+
+    private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        outputStream.close();
+    }
+}
